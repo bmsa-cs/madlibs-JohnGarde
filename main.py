f518587e8d1916c1f0d36898ddf9e495f678b3f6
John Garden <jgarden24@biomedscienceacademy.org>
2020-11-06 20:55:57 +0000

    I started writing a story for the madlib and added some variables for the story as well.  I plan on working on this over the weekend, but this is what I have for right now.

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 """
 MadLibs
-Author: 
-Period/Core:
-
-
+Author: John Garden
+Period/Core: 5
+What I have so far is below, though I plan to work on it.
 """
+
+print("Let's play Silly Sentences!")
+#variables
+adj_a = input("Enter an adjective: ")
+verb_a = input("Enter a verb ending in \"ly\": ")
+before_after = input("Before or after? ")
+cereala = input("Enter a brand of cereal: ")
+song_name = input("Enter a song title: ")
+band_name = input("Enter a band name or artist: ") 
+band_name_b = input("Enter a band name or artist: ")
+obj_a = input("Enter an object: ")
+magic = input("Enter a magical place: ")
+place_a = input("Enter a name of a place: ")
+name = input("Enter a cool nickname: ")
+mood = input("Enter an emotion: ")
+place_b = input("Enter a name of a place: ")
+name_b = input("Enter a name: ")
+verb_b = input("Enter a verb: ")
+#variables
+#story
+print("     It was a " + adj_a +" day when I woke up.  The stars had ended their nightshift and it was time for me to begin my day.  Nothing spectacular was happening today, so I walked "+verb_a+" down the stairs to pour my milk " + before_after + " adding my cereal, " + cereala + ".  I put in my earbuds and turned on \"" + song_name + "\" by Bob Marley.  After finishing my breakfast, I headed up stairs to put on some jeans and a " + band_name + " shirt.  I wanted to match with the "+band_name_b+ + obj_a + " I'd be bringing to school.  When I arrived at school, I noticed that everything looked droopy.  A rift in space time opened up and the " + magic+ " Queen came through.  Or atleast that's what she called herself.")
+print("     \"hello, hero.  I have come seeking your expertise in the art of saving the world we call \'" + place_a + ".\'\"")
+print("     \"You have the wrong place,\" I replied to the "+place_a+" Queen as I certianly wasn't the hero she was looking for, \"Go ask around for someone named \""+name+".\"")
+print("She seemed to be fine with this and left through her rift back to her world never to be seen again.  Or atleast for today while she thinks I'm not "+name_b+".  Though it was odd she didn't stick around to look for "+name+" and left really quickly.  I realized I was late for class and so I began to "+verb_b+" to class to be on time.  ")
+#story
